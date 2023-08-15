fdac22e6f0a6eafa63e24b88921c69ec0a29511c
Talles_Souza <tallesdelfino1@gmail.com>
2023-08-15 00:09:52 -0300

    adicionando dark mode e root

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,94 @@
-import React from 'react';
-import './global.css'
+import React, { useEffect, useState } from 'react';
 import SignUp from './views/login/index.jsx'
-
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './style/GlobalStyle.js';
+import { darkTheme, lightTheme } from './utils/themeColors.js';
+import { Root } from './routes/routes.jsx';
 function App() {
 
+
+  const savedDarkMode = localStorage.getItem("darkMode");
+  const [darkMode, setDarkMode] = useState(savedDarkMode === "true");
+  const [size, setSize] = useState(() => {
+    const savedFontSize = localStorage.getItem("fontSize");
+    return savedFontSize ? parseInt(savedFontSize) : 20;
+  });
+
+  const [increaseCount, setIncreaseCount] = useState(() => {
+    const savedIncreaseCount = localStorage.getItem("increaseCount");
+    return savedIncreaseCount ? parseInt(savedIncreaseCount) : 0;
+  });
+
+  const [decreaseCount, setDecreaseCount] = useState(() => {
+    const savedDecreaseCount = localStorage.getItem("decreaseCount");
+    return savedDecreaseCount ? parseInt(savedDecreaseCount) : 0;
+  });
+
+  const maxCount = 3;
+
+  useEffect(() => {
+    localStorage.setItem("fontSize", size.toString());
+  }, [size]);
+
+  useEffect(() => {
+    localStorage.setItem("increaseCount", increaseCount.toString());
+  }, [increaseCount]);
+
+  useEffect(() => {
+    localStorage.setItem("decreaseCount", decreaseCount.toString());
+  }, [decreaseCount]);
+  useEffect(() => {
+    localStorage.setItem("fontSize", size.toString());
+  }, [size]);
+
+  const increaseFontSize = () => {
+    if (increaseCount < maxCount) {
+      setSize((prevSize) => prevSize + 2);
+      setIncreaseCount((prevCount) => prevCount + 1);
+
+      if (increaseCount + 1 === maxCount) {
+        setDecreaseCount(0);
+      }
+    }
+  };
+
+  const decreaseFontSize = () => {
+    if (decreaseCount < maxCount) {
+      setSize((prevSize) => prevSize - 2);
+      setDecreaseCount((prevCount) => prevCount + 1);
+
+      if (decreaseCount + 1 === maxCount) {
+        setIncreaseCount(0);
+      }
+    }
+  };
+  useEffect(() => {
+    const savedFontSize = localStorage.getItem("fontSize");
+    if (savedFontSize) {
+      setSize(parseInt(savedFontSize));
+    }
+    localStorage.setItem("darkMode", darkMode.toString());
+  }, [darkMode]);
+
+  const HandledarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+
+  const theme = {
+    ...(darkMode ? darkTheme : lightTheme),
+    font: {
+      size: `${size}px`,
+    },
+  };
+
+  const checar = darkMode ? true : false;
+
   return (
-    
-      <SignUp/>
-  
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Root />
+    </ThemeProvider>
   )
 }
 
